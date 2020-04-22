commercialfreejazz/tasking-manager

Register new users with email feature

migrations/versions/5eba4824505e_.py
@@ -1,0 +1,38 @@
+"""empty message
+
+Revision ID: 5eba4824505e
+Revises: d2e18f0f34a9
+Create Date: 2019-10-22 14:17:35.925047
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "5eba4824505e"
+down_revision = "d2e18f0f34a9"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.create_table(
+        "users_with_email",
+        sa.Column("id", sa.BigInteger(), nullable=False),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("email"),
+    )
+    op.create_index(
+        op.f("ix_users_with_email_id"), "users_with_email", ["id"], unique=False
+    )
+    # ### end Alembic commands ###
+
+
+def downgrade():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.drop_index(op.f("ix_users_with_email_id"), table_name="users_with_email")
+    op.drop_table("users_with_email")
+    # ### end Alembic commands ###

escarabajo-rinoceronte/gestor-tareas-pruebas

migrations/versions/5eba4824505e_.py
@@ -1,0 +1,38 @@
+"""empty message
+
+Revision ID: 5eba4824505e
+Revises: f86698c827cc
+Create Date: 2019-10-22 14:17:35.925047
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "5eba4824505e"
+down_revision = "f86698c827cc"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.create_table(
+        "users_with_email",
+        sa.Column("id", sa.BigInteger(), nullable=False),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("email"),
+    )
+    op.create_index(
+        op.f("ix_users_with_email_id"), "users_with_email", ["id"], unique=False
+    )
+    # ### end Alembic commands ###
+
+
+def downgrade():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.drop_index(op.f("ix_users_with_email_id"), table_name="users_with_email")
+    op.drop_table("users_with_email")
+    # ### end Alembic commands ###